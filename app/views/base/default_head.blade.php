fec37cdfa7129e8e3ed5bf4006e9fd5d56c2a4f4
Sarawut Popadcha <rensamatar@gmail.com>
2014-11-06 21:39:19 +0700

    init project

---
+++ app/views/base/default_head.blade.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Laravel Prototype</title>
+{{ HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css') }}
+{{ HTML::style('assets/css/main.css') }}
+
+{{ HTML::script('assets/js/jquery-1.11.1.min.js') }}
+{{ HTML::script('assets/js/jquery-migrate-1.2.1.min.js') }}
+{{ HTML::script('assets/js/application.js')}}
---
+++ app/views/layout/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	@include('base.default_head')
+</head>
+<body>
+	<header>
+
+	</header>
+	<div id="content">
+		@yield('base-content')
+	</div>
+	<footer>
+		@include('base.default_footer')
+	</footer>
+</body>
+</html>
